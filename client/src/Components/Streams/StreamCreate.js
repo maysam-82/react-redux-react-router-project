d685ca6e71701c6998a388e9ec4f44d2f9fb2446
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-17 01:36:09 +1000

    implement handle form submission

--- client/src/Components/Streams/StreamCreate.js
+++ client/src/Components/Streams/StreamCreate.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+class StreamCreate extends Component {
+	// `formProps`  is props which has come from redux-form and container value and event handler such as onChange which we used in React without redux.
+	renderInput = ({ input, label }) => {
+		// `{...input} will take formProps.input property and take all key value pairs inside this object and add them as properties to the input element `
+		return (
+			<div className="field">
+				<label htmlFor="">{label}</label>
+				<input {...input} />
+			</div>
+		);
+	};
+	// `handleSubmit` will take care of event object in Redux-Form.
+	// formValues or whatever value exits inside form element inputs.
+	onSubmitFormHandler = (formValues) => {};
+	render() {
+		return (
+			<form
+				className="ui form"
+				// `handleSubmit` will take care of event object  in Redux-Form.
+				onSubmit={this.props.handleSubmit(this.onSubmitFormHandler)}
+			>
+				{/* label is an extra prop we add to Field component */}
+				<Field name="title" component={this.renderInput} label="Enter Title" />
+				<Field
+					name="description"
+					component={this.renderInput}
+					label="Enter Description"
+				/>
+				<button className="ui button primary">Submit</button>
+			</form>
+		);
+	}
+}
 
-const StreamCreate = () => {
-	return <div>Stream Create</div>;
-};
-
-export default StreamCreate;
+export default reduxForm({
+	form: 'streamCreate',
+})(StreamCreate);
