2289d7a46dcacf7125bb9244d247bcd7960a2eeb
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-17 00:56:35 +1000

    implement redux form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+// import default reducer which has been already created by redux-form
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 export default combineReducers({
 	auth: authReducer,
+	form: formReducer,
 });
