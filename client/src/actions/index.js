8a7c8a62efea98a46384317f073d583963277892
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-17 22:53:18 +1000

    add new action creators for streams

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -18,3 +18,31 @@ export const createStream = (formValues) => async (dispatch) => {
 		payload: response.data,
 	});
 };
+export const fetchStreams = () => async (dispatch) => {
+	const response = await axios.get('/streams');
+	dispatch({
+		type: actionTypes.FETCH_STREAMS,
+		payload: response.data,
+	});
+};
+export const fetchStream = (streamId) => async (dispatch) => {
+	const response = await axios.get(`/streams/:${streamId}`);
+	dispatch({
+		type: actionTypes.FETCH_STREAM,
+		payload: response.data,
+	});
+};
+export const deleteStreams = (streamId) => async (dispatch) => {
+	await axios.get(`/streams/:${streamId}`);
+	dispatch({
+		type: actionTypes.DELETE_STREAM,
+		payload: streamId,
+	});
+};
+export const editStream = (streamId, streamValues) => async (dispatch) => {
+	const response = await axios.put(`/streams/:${streamId}`, streamValues);
+	dispatch({
+		type: actionTypes.EDIT_STREAM,
+		payload: response.data,
+	});
+};
