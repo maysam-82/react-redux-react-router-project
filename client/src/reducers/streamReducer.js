4123b4dd41a0f9523a35a38e9d97b71ed5b06386
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-17 23:47:30 +1000

    refactor streamReducer

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import * as actionTypes from '../actions/actionTypes';
 
+export const setStreamToNewState = (state, action) => {
+	return { ...state, [action.payload.id]: action.payload };
+};
+
 export default (state = {}, action) => {
 	switch (action.type) {
 		case actionTypes.CREATE_STREAM:
@@ -8,18 +12,18 @@ export default (state = {}, action) => {
 			//  const newState = {...state};
 			// newState[action.payload.id] = action.payload;
 			// return newState
-			return { ...state, [action.payload.id]: action.payload };
+			return setStreamToNewState(state, action);
 		case actionTypes.FETCH_STREAMS:
 			// using lodash mapKeys method which taks an array and return an object. They key for each object will be set in the argument.
 			return { ...state, ..._.mapKeys(action.payload, 'id') };
 
 		case actionTypes.EDIT_STREAM:
-			return { ...state, [action.payload.id]: action.payload };
+			return setStreamToNewState(state, action);
+		case actionTypes.FETCH_STREAM:
+			return setStreamToNewState(state, action);
 		case actionTypes.DELETE_STREAM:
 			// using omit method which located inside lodash package to delete a property from state with an id
 			return _.omit(state, action.payload);
-		case actionTypes.FETCH_STREAMS:
-			return { ...state, [action.payload.id]: action.payload };
 		default:
 			return state;
 	}
