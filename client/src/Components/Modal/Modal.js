bfce092ee9ef9907daf6bd3830b07a214f86b34e
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-18 18:05:18 +1000

    make modal reusable

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -1,25 +1,18 @@
 // Creating Modal with React Portal
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../../history';
 
-const Modal = (props) => {
+const Modal = ({ title, description, actions, onDismissed }) => {
 	return ReactDOM.createPortal(
-		<div
-			className="ui dimmer modals visible active"
-			onClick={() => history.push('/')}
-		>
+		<div className="ui dimmer modals visible active" onClick={onDismissed}>
 			{/* (event)=> event.stopPropagation() to avoid event bubbling called from child to parent and dismiss the form while clicking not on overlay. */}
 			<div
 				onClick={(event) => event.stopPropagation()}
 				className="ui standard modal visible active"
 			>
-				<div className="header">Delete Stream</div>
-				<div className="content">Are you sure to delete this stream?</div>
-				<div className="actions">
-					<button className="ui button negative">Delete</button>
-					<button className="ui button">Cancel</button>
-				</div>
+				<div className="header">{title}</div>
+				<div className="content">{description}</div>
+				<div className="actions">{actions}</div>
 			</div>
 		</div>,
 		document.querySelector('#modal')
